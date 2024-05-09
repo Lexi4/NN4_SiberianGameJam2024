282fa66b9421850111e67f958259125800b53802
lexi4 <mr.alyxey13@gmail.com>
2024-05-10 02:41:47 +0300

    Add a* pathfinding. Add basic AI classes WIP. Move game content to Game folder to separate with other packages

---
+++ Assets/Game/Scripts/AI/BaseEnemyAI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace AI
+{
+    public class BaseEnemyAI : MonoBehaviour
+    {
+        [SerializeField] protected float patrolRadius = 2.0f;
+        [SerializeField] protected IEnumerator defaultBehaviour;
+        
+        protected GameObject target = null;
+
+        protected virtual void Start()
+        {
+            StartCoroutine(defaultBehaviour);
+        }
+        
+        //Should used only outside behaviour methods
+        public void ForceBehaviour(IEnumerator behaviour)
+        {
+            StopAllCoroutines();
+            StartCoroutine(behaviour);
+        }
+
+        protected virtual void OnDestroy()
+        { 
+            StopAllCoroutines();
+        }
+    }
+}
---
+++ Assets/Game/Scripts/AI/EnemyRegularAI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace AI
+{
+    public class EnemyRegularAI : BaseEnemyAI
+    {
+        public EnemyRegularAI()
+        {
+            defaultBehaviour = Behaviour_Patrol();
+        }
+        private void OnTriggerEnter (Collider other)
+        {
+            Debug.Log ($"Collided with: {other.gameObject.name}");
+            
+            if (!other.gameObject.CompareTag("Player")) return;
+            target = other.gameObject;
+            ForceBehaviour(Behaviour_Chasing());
+        }
+
+        private void OnTriggerStay (Collider other)
+        {
+            Debug.Log ($"A {other.gameObject.name} is inside the DoorObject trigger");
+        }
+
+        private void OnTriggerExit (Collider other)
+        {
+            Debug.Log ($"A {other.gameObject.name} has exited the DoorObject trigger");
+            ForceBehaviour(Behaviour_Patrol());
+        }
+
+        public IEnumerator Behaviour_Patrol()
+        {
+            //Walk around. Search something (player)
+            yield break;
+        }
+        
+        public IEnumerator Behaviour_RunAway()
+        {
+            //Play anim. Find target position away from light 
+            yield break;
+        }
+        
+        public IEnumerator Behaviour_Chasing()
+        {
+            //Go for player
+            
+            yield break;
+        }
+    }
+}
