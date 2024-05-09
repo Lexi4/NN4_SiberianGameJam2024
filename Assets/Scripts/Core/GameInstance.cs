16cef75073aa5a7ee830dedf5114476cd1b78a5d
Gekata-2 <sosorii2002@gmail.com>
2024-05-09 12:14:58 +0300

    fix

--- Assets/Scripts/Core/GameInstance.cs
+++ Assets/Scripts/Core/GameInstance.cs
@@ -1,19 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GameInstance : MonoBehaviour
+namespace Core
 {
-    [SerializeFiled] private float zxc;
-    // Start is called before the first frame update
-    void Start()
+    public class GameInstance : MonoBehaviour
     {
+        [SerializeField] private float zxc;
+        // Start is called before the first frame update
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
     }
 }
